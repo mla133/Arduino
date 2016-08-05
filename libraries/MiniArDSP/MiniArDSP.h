366cd1e3ded9515312a9e45507168c07aae55c3d
Matt Allen <matthew.allen@fmcti.com>
2016-08-05 16:20:48 -0400

    Adding in MiniArDSP and some examples.

---
+++ libraries/MiniArDSP/MiniArDSP.h
@@ -0,0 +1,14 @@
+// MiniArDSP.h - a basic ardiuno library for doing effects
+// with minimal circuitry added to the arduino
+// guest - openmusiclabs - 1.18.13
+
+#ifndef MiniArDSP_h // include guard
+#define MiniArDSP_h
+
+#include "WProgram.h"
+#include <avr/pgmspace.h>
+#include "mult16x16.h"
+#include "mult16x8.h"
+#include "mult32x16.h"
+
+#endif // end include guard"
